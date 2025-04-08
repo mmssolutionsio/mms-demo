ff4ee872aa789bcbd0b425358ce74c8ab33c088c
saeid.samani <saeid.samani@nsgroup.ch>
2025-04-08 14:18:41 +0200

    SAVE CODE

--- src/Autoload.ts
+++ src/Autoload.ts
@@ -1,14 +1,14 @@
 import ArticleAutoloader from 'nswow/ArticleAutoloader'
 import animatingNumber from "../livingdocs/Projekt/animating-number/app.ts"
-import animatingDiagrams from "../livingdocs/Projekt/animating-diagrams/app.ts"
-import accordion from "../livingdocs/Misc/accordion/app.ts"
 import anchor from "../livingdocs/Misc/anchor/app.ts"
+import accordion from "../livingdocs/Misc/accordion/app.ts"
+import animatingDiagrams from "../livingdocs/Projekt/animating-diagrams/app.ts"
 import nswowTable from "../livingdocs/Media/nswow-table/app.ts"
 const ClassAutoLoader = new ArticleAutoloader()
 ClassAutoLoader.register(animatingNumber, "animatingNumber")
-ClassAutoLoader.register(animatingDiagrams, "animatingDiagrams")
-ClassAutoLoader.register(accordion, "accordion")
 ClassAutoLoader.register(anchor, "anchor")
+ClassAutoLoader.register(accordion, "accordion")
+ClassAutoLoader.register(animatingDiagrams, "animatingDiagrams")
 ClassAutoLoader.register(nswowTable, "nswowTable")
 export default ClassAutoLoader
 export { ClassAutoLoader }
