0c8c5b87859a9cb56607a0e55fe3663f0b360a87
saeid.samani <saeid.samani@nsgroup.ch>
2025-04-15 11:23:25 +0200

    Adjusted image box to vertically center-align the right column.

--- src/Autoload.ts
+++ src/Autoload.ts
@@ -1,14 +1,14 @@
 import ArticleAutoloader from 'nswow/ArticleAutoloader'
 import animatingNumber from "../livingdocs/Projekt/animating-number/app.ts"
-import anchor from "../livingdocs/Misc/anchor/app.ts"
 import animatingDiagrams from "../livingdocs/Projekt/animating-diagrams/app.ts"
-import accordion from "../livingdocs/Misc/accordion/app.ts"
 import nswowTable from "../livingdocs/Media/nswow-table/app.ts"
+import anchor from "../livingdocs/Misc/anchor/app.ts"
+import accordion from "../livingdocs/Misc/accordion/app.ts"
 const ClassAutoLoader = new ArticleAutoloader()
 ClassAutoLoader.register(animatingNumber, "animatingNumber")
-ClassAutoLoader.register(anchor, "anchor")
 ClassAutoLoader.register(animatingDiagrams, "animatingDiagrams")
-ClassAutoLoader.register(accordion, "accordion")
 ClassAutoLoader.register(nswowTable, "nswowTable")
+ClassAutoLoader.register(anchor, "anchor")
+ClassAutoLoader.register(accordion, "accordion")
 export default ClassAutoLoader
 export { ClassAutoLoader }
