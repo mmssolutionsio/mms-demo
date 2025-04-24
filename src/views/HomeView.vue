e75a324ca3052f6c6247b75d38abb1a55cce313d
saeid.samani <saeid.samani@nsgroup.ch>
2025-04-24 11:38:20 +0200

    DIX Home View for INDEX PAGE in app

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,41 +1,58 @@
 <script setup lang="ts">
-import { computed } from 'vue'
+import { computed, nextTick, onMounted, ref } from 'vue'
 import { ArrayToString } from '@/utils/variables'
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
+import Autoload from '@/Autoload.ts'
+import useConfig from '@/composables/config.ts'
+
+
+
 
 const route = useRoute()
+const config = await useConfig()
+
+const articleContent = ref<HTMLDivElement | undefined>()
+const output = ref<string>('')
+const router = useRouter()
 
 const locale = computed<string>(() => {
   return ArrayToString(route.params.locale)
 })
+async function getContent() {
+  const currentPage =
+    config.value.articles[locale.value] &&
+    config.value.articles[locale.value].find((i) => i.index)
+  let res = ''
+  if (currentPage) {
+    const file = `./html/${locale.value}/${currentPage.name}.html`
+    try {
+      const response = await fetch(file)
+      res = await response.text()
+      output.value = res.replaceAll('../','./');
+      await nextTick(() => {
+        if (articleContent.value) {
+          Autoload.init(articleContent.value)
+        }
+      })
+    } catch (e) {
+      console.error(`"${file}" could not be loaded.`)
+      await router.push(`/${locale.value}/404`)
+    }
+  } else {
+    await router.push(`/${locale.value}/404`)
+  }
+}
+onMounted(async () => {
+  await getContent()
+})
 </script>
 
 <template>
-  <div>
-    <div
-      class="srlp-image-container srlp-grid _height-45 srlp-grid__full"
-      style="
-        background-image: url(./images/1912f45f-4a69-4e1c-9d74-9e9cd4f63744_width-1240.jpeg);
-        --xlarge-url: url(./images/1912f45f-4a69-4e1c-9d74-9e9cd4f63744_width-2048.jpeg);
-        --large-url: url(./images/1912f45f-4a69-4e1c-9d74-9e9cd4f63744_width-1280.jpeg);
-        --medium-url: url(./images/1912f45f-4a69-4e1c-9d74-9e9cd4f63744_width-768.jpeg);
-        --small-url: url(./images/1912f45f-4a69-4e1c-9d74-9e9cd4f63744_width-400.jpeg);
-      "
-    >
-      <div class="srlp-grid-child">
-        <div class="srlp-image-container-holder">
-          <div class="srlp-image-container__headline-container"></div>
-          <div class="srlp-image-container__subheadline-container">
-            <p class="srl-paragraph srlp-grid _text-color-cream _text-typo-title-3">
-              <span class="srlp-grid-child">PEOPLE</span>
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-
-
-  </div>
+  <article>
+    <transition name="fade">
+      <div v-if="output.length" ref="articleContent" class="srl-article-root" v-html="output" />
+    </transition>
+  </article>
 </template>
 
 <style scoped lang="scss">
