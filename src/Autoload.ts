7362c9c71d439ae0d5e6ac045cd280b6ff867b55
saeid.samani <saeid.samani@nsgroup.ch>
2025-05-07 02:13:03 +0200

    fix css

--- src/Autoload.ts
+++ src/Autoload.ts
@@ -2,13 +2,13 @@ import ArticleAutoloader from 'nswow/ArticleAutoloader'
 import anchor from "../livingdocs/Misc/anchor/app.ts"
 import accordion from "../livingdocs/Misc/accordion/app.ts"
 import animatingNumber from "../livingdocs/Projekt/animating-number/app.ts"
-import animatingDiagrams from "../livingdocs/Projekt/animating-diagrams/app.ts"
 import nswowTable from "../livingdocs/Media/nswow-table/app.ts"
+import animatingDiagrams from "../livingdocs/Projekt/animating-diagrams/app.ts"
 const ClassAutoLoader = new ArticleAutoloader()
 ClassAutoLoader.register(anchor, "anchor")
 ClassAutoLoader.register(accordion, "accordion")
 ClassAutoLoader.register(animatingNumber, "animatingNumber")
-ClassAutoLoader.register(animatingDiagrams, "animatingDiagrams")
 ClassAutoLoader.register(nswowTable, "nswowTable")
+ClassAutoLoader.register(animatingDiagrams, "animatingDiagrams")
 export default ClassAutoLoader
 export { ClassAutoLoader }
